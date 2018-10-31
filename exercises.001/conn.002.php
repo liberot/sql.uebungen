861172893f3a97b205fe6036b136a3e4055c8686
liberot <vberzsin@web.de>
2018-10-31 18:28:45 +0100

    batch insert of external data sources

--- exercises.001/conn.002.php
+++ exercises.001/conn.002.php
@@ -3,93 +3,109 @@
 	uses ddl.001.root.sql 
 	sqlexcdb
 
+	fills mockup db with entries from extern json data sources
  */
 
 require_once('conn.util.php');
 
-// loads a json
-$doc = file_get_contents('artist.000.json');
+function proc($files){
 
-// 
-$coll = json_decode($doc);
+	foreach ($files as $file) {
+	
+		// loads a json
+		// s$doc = file_get_contents('artist.000.json');
+		if(false == ($doc = @file_get_contents($file))){
+			print "not a file: ".$file."\n";
+			continue;
+		}
 
-// mime type for mockup
-$mime_id = q(c(), "call sqlexcdb.init_mimetype('image/png', '')");
+		// 
+		if(false == ($coll = json_decode($doc))){
+			print "not a json: ".$file."\n";
+			continue;
+		}
 
-// timezone for mockup reasons 
-$timezone_id = q(c(), "call sqlexcdb.init_timezone('America/Los Angeles', '')");
+		// mime type for mockup
+		$mime_id = q(c(), "call sqlexcdb.init_mimetype('image/png', '')");
 
-// adds an artist into the db
-$artist_id = q(c(), "call sqlexcdb.init_artist(
-	'".$coll->artist->identification."', 
-	'".$coll->artist->description."'
-	)"
-);
+		// timezone for mockup reasons 
+		$timezone_id = q(c(), "call sqlexcdb.init_timezone('America/Los Angeles', '')");
 
-// adds assets of the artist into the db
-foreach($coll->artist->assets as $asset){
-	$asset_id = q(c(), "call sqlexcdb.init_asset(
-		'".$asset->title."', 
-		'description', 
-		'".$asset->path."', 
-		'".$mime_id."'
-		)"
-	);
-	// links asset to artist
-	$link_id = q(c(), "call sqlexcdb.link_asset_to_artist(
-		'".$asset_id."', 
-		'".$artist_id."', 
-		'meta', 
-		'title', 
-		'description'
-		)"
-	);
-}
-
-// adds releases
-foreach ($coll->artist->releases as $release) {
-	// print_r($release);
-	// date for mockup reasons
-	$d = date("Y-m-d H:i:s");
-	// inits a release
-	$release_id = q(c(), "call sqlexcdb.init_release(
-		'".$release->title."', 
-		'".$release->description."',
-		'".$d."',
-		'".$timezone_id."'
-		)"
-	);
-	// links release
-	$link_id = q(c(), "call sqlexcdb.link_release_to_artist(
-		'".$release_id."', 
-		'".$artist_id."'
-		)"
-	);
-	// inits assets
-	foreach($release->assets as $asset){
-		// inits asset
-		$asset_id = q(c(), "call sqlexcdb.init_asset(
-			'".$asset->title."', 
-			'description', 
-			'".$asset->path."', 
-			'".$mime_id."'
+		// adds an artist into the db
+		$artist_id = q(c(), "call sqlexcdb.init_artist(
+			'".$coll->artist->identification."', 
+			'".$coll->artist->description."'
 			)"
 		);
-		// links asset to release 
-		$link_id = q(c(), "call sqlexcdb.link_asset_to_release(
-			'".$asset_id."', 
-			'".$release_id."', 
-			'meta', 
-			'title', 
-			'description'
-			)"
-		);				
+
+		// adds assets of the artist into the db
+		foreach($coll->artist->assets as $asset){
+			$asset_id = q(c(), "call sqlexcdb.init_asset(
+				'".$asset->title."', 
+				'description', 
+				'".$asset->path."', 
+				'".$mime_id."'
+				)"
+			);
+			// links asset to artist
+			$link_id = q(c(), "call sqlexcdb.link_asset_to_artist(
+				'".$asset_id."', 
+				'".$artist_id."', 
+				'meta', 
+				'title', 
+				'description'
+				)"
+			);
+		}
+
+		// adds releases
+		foreach ($coll->artist->releases as $release) {
+			// print_r($release);
+			// date for mockup reasons
+			$d = date("Y-m-d H:i:s");
+			// inits a release
+			$release_id = q(c(), "call sqlexcdb.init_release(
+				'".$release->title."', 
+				'".$release->description."',
+				'".$d."',
+				'".$timezone_id."'
+				)"
+			);
+			// links release
+			$link_id = q(c(), "call sqlexcdb.link_release_to_artist(
+				'".$release_id."', 
+				'".$artist_id."'
+				)"
+			);
+			// inits assets
+			foreach($release->assets as $asset){
+				// inits asset
+				$asset_id = q(c(), "call sqlexcdb.init_asset(
+					'".$asset->title."', 
+					'description', 
+					'".$asset->path."', 
+					'".$mime_id."'
+					)"
+				);
+				// links asset to release 
+				$link_id = q(c(), "call sqlexcdb.link_asset_to_release(
+					'".$asset_id."', 
+					'".$release_id."', 
+					'meta', 
+					'title', 
+					'description'
+					)"
+				);				
+			}
+		}
 	}
 }
 
+proc(['artist.000.json', 'artist.001.json']);
+
 exit();
 
-/*
+/*	
 select asset.title, asset.path 
 	from asset, asset_to_release
 		where asset.id = asset_to_release.asset_id
