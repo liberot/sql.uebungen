efdf290cb13d7a42290e491286b12b34fca08149
liberot <vberzsin@web.de>
2018-11-16 21:11:03 +0100

    sort of select statements

--- klausur/fsck.004.sql
+++ klausur/fsck.004.sql
@@ -6,14 +6,14 @@ select
 	k.firma,
 	sum(art.listenpreis *p.menge)
 from
-	kunde k,
 	angebot a,
+	kunde k,
 	position p,
 	artikel art
 where
-	a.erstellungsdatum >= str_to_date('2010-10-01', '%Y-%m-%d')
+	a.kundennummer = k.kundennummer
+	and a.erstellungsdatum >= str_to_date('2010-10-01', '%Y-%m-%d')
 	and a.erstellungsdatum <= str_to_date('2022-10-31', '%Y-%m-%d')
-	and a.kundennummer = k.kundennummer
 	and a.angebotsnummer = p.angebotsnummer
 	and p.artikelnummer = art.artikelnummer
 group by
