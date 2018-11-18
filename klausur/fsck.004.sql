c8be2bac4eeed2d0f0c29b50c3b5f405e9cfd326
liberot <vberzsin@web.de>
2018-11-18 20:31:52 +0100

    ordered select statement

--- klausur/fsck.004.sql
+++ klausur/fsck.004.sql
@@ -2,11 +2,37 @@ use klausur;
 
 select
 	a.angebotsnummer,
+	k.nachname,
+	k.firma,
+	art.bezeichnung,
+	art.listenpreis,
+	p.menge,
+	art.listenpreis *p.menge as netto,
+	round((art.listenpreis *p.menge) /100 *19, 2) as ust,
+	round((art.listenpreis *p.menge) +((art.listenpreis *p.menge) /100 *19), 2) as brutto
+from
+	angebot a,
+	kunde k,
+	position p,
+	artikel art
+where
+	a.kundennummer = k.kundennummer
+	and a.erstellungsdatum >= str_to_date('2010-10-01', '%Y-%m-%d')
+	and a.erstellungsdatum <= str_to_date('2022-10-31', '%Y-%m-%d')
+	and a.angebotsnummer = p.angebotsnummer
+	and p.artikelnummer = art.artikelnummer
+order by
+	a.angebotsnummer
+;
+
+
+select
+	a.angebotsnummer,
 	a.erstellungsdatum,
 	k.vorname, 
 	k.nachname,
 	k.firma,
-	sum(art.listenpreis *p.menge)
+	sum(art.listenpreis *p.menge) as gesamt
 from
 	angebot a,
 	kunde k,
@@ -21,3 +47,4 @@ where
 group by
 	a.angebotsnummer
 ;
+
