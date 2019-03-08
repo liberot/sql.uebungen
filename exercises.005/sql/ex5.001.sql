22b0e59cfc4d6bfcff70ca263b27e4b6ef44fc12
liberot <vberzsin@web.de>
2019-03-08 19:59:21 +0100

    sql aufgaben addesql aufgaben addedd

--- exercises.005/sql/ex5.001.sql
+++ exercises.005/sql/ex5.001.sql
@@ -98,7 +98,51 @@ insert
 	values( 6, 904, 2018-01-01, 2018-09-01, "Urlaub", 6 )
 ;;;
 
+update 
+	Fehlzeit
+	set
+		Fehlzeit.Bis_Datum = 2017-18-11,
+		Fehlzeit.Grund = "Dienstreise",
+		Fehlzeit.Fehltage = 2
+	where
+		Fehlzeit.FZ_ID = 4
+;;
 
+select 
+	Mitarbeiter.MA_ID,
+	Mitarbeiter.Nachname,
+	SUM(Fehlzeit.Fehltage)
+from
+	Mitarbeiter
+	left join Fehlzeit on Mitarbeiter.MA_ID = Fehlzeit.MA_ID
+where
+	Fehlzeit.Grund = "Urlaub"
+;;;
+
+drop table Fehlzeit;
+;;;
+
+create table Fehlzeitgrund (
+	Grund_ID int,
+	Grund char(255),
+	primary key(Grund_ID)
+);
+describe Fehlzeitgrund;
+;;; 
+
+
+drop table if exists Fehlzeit;
+create table Fehlzeit(
+	Fehlzeit.MA_ID integer,
+	Fehlzeit.Von_Datum date,
+	Fehlzeit.Bis_Datum date,
+	Fehlzeit.Grund_ID int,
+	Fehlzeit.Fehltage int,
+	primary key Fehlzeit(MA_ID),
+	foreign key Fehlzeit(Grund_ID) references Fehlzeitgrund(Grund_ID)
+);
+describe Fehlzeit;
+;;;
 
 
 
