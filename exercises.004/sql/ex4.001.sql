316ee5d94b5063a6a5c2fc676eef1092c2ba447b
liberot <vberzsin@web.de>
2019-03-08 18:24:37 +0100

    bugfixe

--- exercises.004/sql/ex4.001.sql
+++ exercises.004/sql/ex4.001.sql
@@ -5,10 +5,12 @@
 
 
 drop database if exists eco_pharmalog;
-create database eco_pharmalog(
+create database eco_pharmalog
 	character set utf8mb4 
 	collate utf8mb4_unicode_ci
-);
+;
+
+use eco_pharmalog;
 
 drop table if exists Kunde;
 create table Kunde (
@@ -20,30 +22,30 @@ create table Kunde (
 	primary key(Kunde_ID)
 );
 
-drop table if exists Artikel;
-create table Artikel (
-	Artikel_ID int,
-	Hersteller_ID int,
-	Bezeichnung char(255),
-	Listenpreis decimal(10,2),
-	primary_key(Artikel_ID),
-	foreign_key(Hersteller_ID) references Hersteller(Hersteller_ID)
+drop table if exists Rechnung;
+create table Rechnung (
+	Rechnung_ID int,
+	Kunde_ID int,
+	RechnungsDatum date,
+	primary key(Rechnung_ID, Kunde_ID),
+	foreign key(Kunde_ID) references Kunde(Kunde_ID)
 );
 
 drop table if exists Hersteller;
 create table Hersteller (
 	Hersteller_ID int,
 	Firma char(255),
-	primary_key(Hersteller_ID)
+	primary key(Hersteller_ID)
 );
 
-drop table if exists Rechnung;
-create table Rechnung (
-	Rechnung_ID int,
-	Kunde_ID int,
-	RechnungsDatum date,
-	primary_key(Rechnung_ID, Kunde_ID),
-	foreign_key(Kunde_ID) references Kunde(Kunde_ID)
+drop table if exists Artikel;
+create table Artikel (
+	Artikel_ID int,
+	Hersteller_ID int,
+	Bezeichnung char(255),
+	Listenpreis decimal(10,2),
+	primary key(Artikel_ID),
+	foreign key(Hersteller_ID) references Hersteller(Hersteller_ID)
 );
 
 drop table if exists Position;
@@ -52,14 +54,24 @@ create table Position (
 	Artikel_ID int,
 	Menge int,
 	Verkaufs_Einzelpreis decimal(10,2),
-	primary_key(Rechnung_ID, Artikel_ID),
-	foreign_key(Rechnung_ID) references Rechnung(Rechnung_ID),
-	foreign_key(Artikel_ID) references Artikel(Artikel_ID)
+	primary key(Rechnung_ID, Artikel_ID),
+	foreign key(Rechnung_ID) references Rechnung(Rechnung_ID),
+	foreign key(Artikel_ID) references Artikel(Artikel_ID)
 );
 
 insert into Kunde
 	(Kunde_ID, Firma, Strasse, Plz, Ort)
 	values(0, "Firma1st", "Strasse1st", "Plz1st", "Ort1st")
+;
+
+insert into Hersteller
+	(Hersteller_ID, Firma)
+	values(0, "Firma1st")
+;
+
+insert into Hersteller
+	(Hersteller_ID, Firma)
+	values(1, "Firma2nd")
 ;
 
 insert into Artikel
@@ -75,16 +87,6 @@ insert into Artikel
 insert into Artikel
 	(Artikel_ID, Hersteller_ID, Bezeichnung, Listenpreis)
 	values(2, 0, "Bezeichnung2nd", "12,00")
-;
-
-insert into Hersteller_ID
-	(Hersteller_ID, Firma)
-	values(0, "Firma1st")
-;
-
-insert into Hersteller_ID
-	(Hersteller_ID, Firma)
-	values(1, "Firma2nd")
 ;
 
 insert into Rechnung
@@ -104,7 +106,7 @@ insert into Position
 
 insert into Position
 	(Rechnung_ID, Artikel_ID, Menge, Verkaufs_Einzelpreis)
-	value(0, 3, 10, "30,12")
+	value(0, 2, 10, "30,12")
 ;
 
 
